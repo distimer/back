525602dc686f6975d42e5bdf1677bdb65974e3ac
펜타곤 <seonwoo0808@pentag.kr>
2024-08-17 01:19:36 +0900

    💬 Modify category limit

--- configs/constant.go
+++ configs/constant.go
@@ -2,7 +2,7 @@ package configs
 
 const (
 	FreePlanGroupJoinLimit          = 10
-	FreePlanCategoryLimit           = 10
+	FreePlanCategoryLimit           = 11
 	FreePlanSubjectPerCategoryLimit = 10
 	InviteCodeLength                = 7
 )
